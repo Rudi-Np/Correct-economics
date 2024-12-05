7137b1f4baccac4d832e2b6990c85f69196f0c7c
baily <br166148@gmail.com>
2024-12-05 16:30:21 +0200

    update changes

--- Python/Classes/expenses.py
+++ Python/Classes/expenses.py
@@ -1,20 +1,45 @@
-from Account import Account
+from account import Account
 
 class Expenses:
     ExpensesGlobalId=0
     def __init__(self,sum,date,type,pymentMethod,category):
         Expenses.ExpensesGlobalId+=1
         self.id=Expenses.ExpensesGlobalId
+        self.expenses = [] 
+
         
 
-    def AddExpenses(self,sum,date,type,pymentMethod,category):
-        self.sum=sum
-        self.date=date
-        self.type=type
-        self.pymentMethod=pymentMethod
-        self.category=category
-        Account.globalSum-=sum
+    def AddExpenses(self,amount,date,type,pymentMethod,category):
+       expense_entry = {
+            'id': self.id,
+            'amount': amount,
+            'date': date,
+            'type': type,
+            'pymentMethod':pymentMethod,
+            'category':category
+        } 
+       Account.globalSum-=amount  
+       self.expenses.append(expense_entry) 
+        
 
+    def get_all_expenses(self):
+        return self.expenses
+
+    def get_expense_by_date(self,date):
+        return [expense for expense in self.expenses if expense['date'] == date]
+    
+    def get_expense_by_month(self,month):
+        return[expense for expense in self.expenses if expense['date'].month == month]
+    
+    def get_expense_by_year(self,year):
+        return[expense for expense in self.expenses if expense['date'].year == year]
+    
+    def get_expense_by_type(self,type):
+        return[expense for expense in self.expenses if expense['type'] == type]
+    
+    def get_expense_by_category(self,category):
+        return[expense for expense in self.expenses if expense['category'] == category]
+    
 
 
         
--- Python/Classes/revenues.py
+++ Python/Classes/revenues.py
@@ -1,4 +1,4 @@
-from Account import Account
+from account import Account
 
 class Revenues:
     RevenuesGlobalId = 0
