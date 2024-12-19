ede4386a3d410c03b808d7ec85fb80f7bcbd0dc3
baily <br166148@gmail.com>
2024-12-19 17:53:05 +0200

    a

--- Python/Api/server.py
+++ Python/Api/server.py
@@ -1,4 +1,4 @@
-t addfrom flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_cors import CORS
